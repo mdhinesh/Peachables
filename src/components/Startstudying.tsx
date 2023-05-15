12c910d7320bc64597b6ce7fed1f9995af3d3113
mdhinesh <mdhinesh3103@gmail.com>
2023-05-16 02:27:43 +0530

    changes

--- src/components/Startstudying.tsx
+++ src/components/Startstudying.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchQuizList } from '../api/quizlist';
-import '../styles/startstudying.css';
+import '../styles/Startstudying.css';
 
 const StartStudying = () => {
 
