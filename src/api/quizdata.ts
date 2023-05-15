068c73b01ef3b5e0f115ce14c66910577d9273be
mdhinesh <mdhinesh3103@gmail.com>
2023-05-16 01:28:12 +0530

    api issue solved

--- src/api/quizdata.ts
+++ src/api/quizdata.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const quizApi = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://api-quiz-production.up.railway.app/',
 });
 
 export const fetchQuizData = async (quizname: string) => {
